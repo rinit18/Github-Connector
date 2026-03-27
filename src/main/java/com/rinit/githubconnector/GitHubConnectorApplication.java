3b3768ac0de7149a2fa7a99b182ae4a06bdb01cc
Rinit Bhowmick <rbbsl13@gmail.com>
2026-03-27 15:15:32 +0530

    first commit

---
+++ src/main/java/com/rinit/githubconnector/GitHubConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.rinit.githubconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinit/githubconnector/GitHubConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinit.githubconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
